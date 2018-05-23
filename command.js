727a998fe8614698f9deab38ce0daa8d39720c5f
Adhyaksa Jaya <adhyaksaj@gmail.com>
2018-05-23 22:13:56 +0700

    Added setText, buttonPressed requests

--- command.js
+++ command.js
@@ -41,6 +41,61 @@ const commands = {
     ws.send(JSON.stringify(data));
     console.log('soundOn request broadcasted');
   },
+
+  setText(ws, { id, content }) {
+    if (!client.clientExists(id)) {
+      const data = {
+        status: 'error',
+        type: 'unauthorized',
+        content: 'setText',
+      };
+
+      ws.send(JSON.stringify(data));
+      console.log(`soundOn request failed. Client unauthorized`);
+    }
+
+    const broadcastData = {
+      type: 'setText',
+      content,
+    };
+
+    const data = {
+      status: 'success',
+      type: 'notification',
+      type: 'setText',
+    };
+
+    client.clusterBroadcast(data);
+    ws.send(JSON.stringify(data));
+    console.log('setText request broadcasted');
+  },
+
+  buttonPressed(ws, { id }) {
+    if (!client.clusterExists(id)) {
+      const data = {
+        status: 'error',
+        type: 'unauthorized',
+        content: 'buttonPressed',
+      };
+
+      ws.send(JSON.stringify(data));
+      console.log(`buttonPressed request failed. Cluster unauthorized`);
+    }
+
+    const broadcastData = {
+      type: 'buttonPressed',
+    };
+
+    const data = {
+      status: 'success',
+      type: 'notification',
+      type: 'buttonPressed',
+    };
+
+    client.clusterBroadcast(data);
+    ws.send(JSON.stringify(data));
+    console.log('buttonPressed request broadcasted');
+  },
 };
 
 module.exports = commands;
